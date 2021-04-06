6fe0498324c27105d40bb4eec3250014f0ad33e4
lisswart <arnoudlisa@gmail.com>
2021-04-06 16:03:21 -0400

    added priority color code functionality but still buggy

--- src/index.js
+++ src/index.js
@@ -9,13 +9,51 @@ const createNewTask = event => {
   event.preventDefault();  //stop form from trying to submit
 
   const newTaskDescription = document.getElementById("new-task-description");
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
+  const priorityValues = document.getElementById('priority-values');
+  const submit = document.getElementById('submit');
   
-  appendNewTask(newTask);
+  submit.addEventListener('click', () => {
+
+    if(priorityValues.value === 'red') {
+      const newTask = document.createElement("li");
+      const del = document.createElement('input');
+      newTask.style = 'color: #f90909';
+      del.type = 'checkbox';
+      newTask.innerText = newTaskDescription.value;
+      appendNewTask(newTask);
+      appendCheckBox(del);
+    } 
+    else if(priorityValues.value === 'yellow') {
+      const newTask = document.createElement("li");
+      const del = document.createElement('input');
+      newTask.style = 'color: yellow';
+      del.type = 'checkbox';
+      newTask.innerText = newTaskDescription.value;
+      appendNewTask(newTask);
+      appendCheckBox(del);
+    }
+    else if(priorityValues.value === 'green') {
+      const newTask = document.createElement("li");
+      const del = document.createElement('input');
+      newTask.style = 'color: #41f909';
+      del.type = 'checkbox';
+      del.value = '';
+      del.id = 'checkbox';
+      newTask.innerText = newTaskDescription.value;
+      appendNewTask(newTask);     
+      appendCheckBox(del);
+    }
+  });
+
   event.target.reset();
 };
+
+const appendCheckBox = box => {
+  document.getElementById('tasks').appendChild(box);
+}
 
 const appendNewTask = task => {
   document.getElementById("tasks").appendChild(task);
 };
+
+
