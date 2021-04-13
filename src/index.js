f6c16067d1811db0ecc8c5f0a31fbfdd9abae106
lisswart <arnoudlisa@gmail.com>
2021-04-13 06:42:26 -0400

    added delete button, fixed priority level code

--- src/index.js
+++ src/index.js
@@ -9,51 +9,34 @@ const createNewTask = event => {
   event.preventDefault();  //stop form from trying to submit
 
   const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
   const priorityValues = document.getElementById('priority-values');
-  const submit = document.getElementById('submit');
-  
-  submit.addEventListener('click', () => {
+  if(priorityValues.value === 'red') {        
+    newTask.style = 'background-color: #f90909';        
+    newTask.innerText = newTaskDescription.value;
+  } 
+  else if(priorityValues.value === 'yellow') {
+    newTask.style = 'background-color: #f7c709';
+    newTask.innerText = newTaskDescription.value;
+  }
+  else if(priorityValues.value === 'green') {
+    newTask.style = 'background-color: #41f909';
+    newTask.innerText = newTaskDescription.value;    
+  }
 
-    if(priorityValues.value === 'red') {
-      const newTask = document.createElement("li");
-      const del = document.createElement('input');
-      newTask.style = 'color: #f90909';
-      del.type = 'checkbox';
-      newTask.innerText = newTaskDescription.value;
-      appendNewTask(newTask);
-      appendCheckBox(del);
-    } 
-    else if(priorityValues.value === 'yellow') {
-      const newTask = document.createElement("li");
-      const del = document.createElement('input');
-      newTask.style = 'color: yellow';
-      del.type = 'checkbox';
-      newTask.innerText = newTaskDescription.value;
-      appendNewTask(newTask);
-      appendCheckBox(del);
-    }
-    else if(priorityValues.value === 'green') {
-      const newTask = document.createElement("li");
-      const del = document.createElement('input');
-      newTask.style = 'color: #41f909';
-      del.type = 'checkbox';
-      del.value = '';
-      del.id = 'checkbox';
-      newTask.innerText = newTaskDescription.value;
-      appendNewTask(newTask);     
-      appendCheckBox(del);
-    }
-  });
-
+  appendNewTask(newTask);
   event.target.reset();
-};
 
-const appendCheckBox = box => {
-  document.getElementById('tasks').appendChild(box);
-}
+  const removeTask = document.createElement('input');
+  removeTask.value = "x";
+  removeTask.type = "button";
+  removeTask.id = "remove-button";
+  removeTask.addEventListener("click", event => newTask.parentNode.removeChild(newTask), false);
+  newTask.appendChild(removeTask);
+};
 
 const appendNewTask = task => {
   document.getElementById("tasks").appendChild(task);
 };
-
-
